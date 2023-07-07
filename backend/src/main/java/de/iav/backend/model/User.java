5a2eb6597de538928e311c3274a93ba4daa9c362
JaroJenaro <jaroslaw.placzek.job@icloud.com>
2023-07-07 17:58:16 +0200

    Changes by hou7man

--- backend/src/main/java/de/iav/backend/model/User.java
+++ backend/src/main/java/de/iav/backend/model/User.java
@@ -1,5 +1,34 @@
 package de.iav.backend.model;
-//record for stocks
 
-public record User() {
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+
+
+@Builder
+@Document(collection= "users")
+
+public record User(
+             String userId,
+                   String firstName,
+                   String lastName,
+                   String email,
+                   String password) {
+
+    /*public User withId(User previousUser, String newId) {
+        return new User(newId, previousUser.firstName, previousUser.lastName, previousUser.email, previousUser.password);
+
+    }
+
+     */
+
+    public User withId( String newId) {
+        return new User(newId, this.firstName, this.lastName, this.email, this.password);
+
+    }
 }
--- backend/src/main/java/de/iav/backend/service/UserService.java
+++ backend/src/main/java/de/iav/backend/service/UserService.java
@@ -1,4 +1,53 @@
 package de.iav.backend.service;
 
+import de.iav.backend.exception.UserNotFoundException;
+import de.iav.backend.model.User;
+
+import de.iav.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+
+
+    public Optional<User> getUserById(String id){
+        return userRepository.findById(id);
+    }
+
+    public List<User> findAllByFirstNameEqualsIgnoreCase(String firstName){
+        return userRepository.findAllByFirstNameEqualsIgnoreCase(firstName);
+    }
+
+    public List<User> findAllByLastNameEqualsIgnoreCase(String lastName){
+        return userRepository.findAllByLastNameEqualsIgnoreCase(lastName);
+    }
+    public void deleteUser(String id){
+        userRepository.deleteById(id);
+    }
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String id, User userToUpdate) {
+        userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+
+        User updatedStudent = userToUpdate.withId(id);
+
+        return userRepository.save(updatedStudent);
+    }
+
 }
