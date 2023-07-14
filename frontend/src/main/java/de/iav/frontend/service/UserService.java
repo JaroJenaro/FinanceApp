eb670b4a50a1b2db183b443a7f2afc3f2414ae53
Houman Mohammadi <houmane.el@gmail.com>
2023-07-14 13:09:45 +0200

    user service von Jaro übernommen

--- frontend/src/main/java/de/iav/frontend/service/UserService.java
+++ frontend/src/main/java/de/iav/frontend/service/UserService.java
@@ -1,60 +1,51 @@
 package de.iav.frontend.service;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.iav.frontend.model.Stock;
-
 import de.iav.frontend.model.User;
-
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
+import java.util.List;
 public class UserService {
-
     private static UserService instance;
-
     private final HttpClient httpClient;
     private final ObjectMapper objectMapper;
-
     private UserService() {
         this.httpClient = HttpClient.newHttpClient();
         this.objectMapper = new ObjectMapper();
     }
-
     public static synchronized UserService getInstance() {
         if (instance == null) {
             instance = new UserService();
         }
         return instance;
     }
-
+    public List<User> getAllUsers() {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://localhost:8080/api/financeapp/users"))
+                .build();
+        return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenApply(this::mapToUserList) // .thenApply(responseBody -> mapToStudent(responseBody))
+                .join();
+    }
+    private List<User> mapToUserList(String responseBody) {
+        try {
+            return objectMapper.readValue(responseBody, new TypeReference<>() {
+            });
+            //return new ArrayList<Student>();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to map users List", e);
+        }
+    }
     private User mapToUser(String json) {
         try {
             return objectMapper.readValue(json, User.class);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to map transaction", e);
+            throw new RuntimeException("Failed to map stock", e);
         }
     }
-
-    public User addUser(User user) {
-        try {
-            String requestBody = objectMapper.writeValueAsString(user);
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("http://localhost:8080/api/financeapp/users"))
-                    .header("Content-Type", "application/json")
-                    .header("Accept", "application/json")
-                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-                    .build();
-
-            return httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                    .thenApply(HttpResponse::body)
-                    .thenApply(this::mapToUser)
-                    .join();
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
