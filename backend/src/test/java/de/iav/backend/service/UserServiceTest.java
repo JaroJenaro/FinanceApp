47545737f2b77950e9b8c7cd91464ff819591d1f
JaroJenaro <jaroslaw.placzek.job@icloud.com>
2023-07-21 23:18:14 +0200

    UserService Konstruktor Aufruf um stockRepository erweitert

--- backend/src/test/java/de/iav/backend/service/UserServiceTest.java
+++ backend/src/test/java/de/iav/backend/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package de.iav.backend.service;
 
 import de.iav.backend.exception.UserNotFoundException;
 import de.iav.backend.model.User;
+import de.iav.backend.repository.StockRepository;
 import de.iav.backend.repository.TransactionRepository;
 import de.iav.backend.repository.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -11,15 +12,19 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+
 class UserServiceTest {
     UserRepository userRepository = mock(UserRepository.class);
     TransactionRepository transactionRepository = mock(TransactionRepository.class);
-    UserService userService = new UserService(userRepository,transactionRepository);
+    StockRepository stockRepository = mock(StockRepository.class);
+
+    UserService userService = new UserService(userRepository, transactionRepository, stockRepository);
+
     @Test
     void getUserById_whenExistingId_thenReturnUser() {
         //GIVEN
         String expectedUserId = "12345";
-        User expectedUser = new User(expectedUserId,  "Erum", "Schuakat", "erum.schaukat@iav.de", "12345");
+        User expectedUser = new User(expectedUserId, "Erum", "Schuakat", "erum.schaukat@iav.de", "12345");
 
         // Wie soll sich das "gemockte" Repository verhalten?
         // Der Dummy soll das expectedProduct liefern, wenn ein Aufruf von getProductById
@@ -31,6 +36,7 @@ class UserServiceTest {
         Optional<User> actualUser = userService.getUserById(expectedUserId);
 
         //THEN
+        assertTrue(actualUser.isPresent());
         assertEquals(expectedUser, actualUser.get());
         // Sicherstellen, dass getProductById auch WIRKLICH aufgerufen wurde
         verify(userRepository).findById(any());
