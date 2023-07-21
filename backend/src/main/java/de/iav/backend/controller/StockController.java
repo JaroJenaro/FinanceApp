d5adc4305d027fe2dd2354f47400776da0e70d7e
Houman Mohammadi <houmane.el@gmail.com>
2023-07-21 17:50:57 +0200

    setAllStockPrices runs 5 sec after application start

--- backend/src/main/java/de/iav/backend/controller/StockController.java
+++ backend/src/main/java/de/iav/backend/controller/StockController.java
@@ -1,10 +1,14 @@
 package de.iav.backend.controller;
 
 
+import de.iav.backend.externalAPICommunication.StockDataAccess;
 import de.iav.backend.model.CurrentStockPrice;
 import de.iav.backend.model.Stock;
 import de.iav.backend.service.CurrentStockPriceService;
 import de.iav.backend.service.StockService;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,25 +20,26 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/financeapp/stocks")
-public class StockController {
+public class StockController implements ApplicationRunner {
 
     private final StockService stockService;
-    private final de.iav.backend.externalAPIcommunication.StockDataAccess stockDataAccess;
+    private final StockDataAccess stockDataAccess;
     private final CurrentStockPriceService currentStockPriceService;
 
 
-    public StockController(StockService stockService, de.iav.backend.externalAPIcommunication.StockDataAccess stockDataAccess, CurrentStockPriceService currentStockPriceService) {
+    public StockController(StockService stockService, StockDataAccess stockDataAccess, CurrentStockPriceService currentStockPriceService) {
         this.stockService = stockService;
         this.stockDataAccess = stockDataAccess;
         this.currentStockPriceService = currentStockPriceService;
     }
 
     @GetMapping
-    public List<Stock> getAllStocks(){return stockService.getAllStocks();
+    public List<Stock> getAllStocks() {
+        return stockService.getAllStocks();
     }
 
     @GetMapping("/{id}")
-    public Optional<Stock> getStockById(@PathVariable String id){
+    public Optional<Stock> getStockById(@PathVariable String id) {
         return stockService.getStockById(id);
     }
 
@@ -42,6 +47,8 @@ public class StockController {
     public List<Stock> setDefaultStocks() {
         return stockService.setStockByRepository();
     }
+
+
 
     @GetMapping("/price/{stockSymbol}")
     public Double getStockPrice(@PathVariable String stockSymbol) {
@@ -52,16 +59,28 @@ public class StockController {
         return currentPrice;
     }
 
-    @GetMapping("/setAllPrice")
-    public String setAllStockPrices() {
-        for (Stock stock : getAllStocks()) {
-            System.out.println(stockDataAccess.getLastPriceForStock(stock.getStockTicker()).toString());
-            Double currentPrice = stockDataAccess.getLastPriceForStock(stock.getStockTicker());
-            CurrentStockPrice currentStockPrice = new CurrentStockPrice(stock.getStockTicker(), currentPrice, LocalDateTime.now().toString());
-            currentStockPriceService.saveCurrentStockPrice(currentStockPrice);
-        }
-
-        return "Alle Preise aktualisiert";
+    @Override
+    public void run(ApplicationArguments args) throws InterruptedException {
+        Thread.sleep(5000);
+        setAllStockPrices();
     }
 
+    @GetMapping("/setAllPrice")
+    public void setAllStockPrices() throws InterruptedException {
+        for (Stock stock : getAllStocks()) {
+            try {
+                System.out.println(stockDataAccess.getLastPriceForStock(stock.getStockTicker()).toString());
+                Double currentPrice = stockDataAccess.getLastPriceForStock(stock.getStockTicker());
+                CurrentStockPrice currentStockPrice = new CurrentStockPrice(stock.getStockTicker(), currentPrice, LocalDateTime.now().toString());
+                currentStockPriceService.saveCurrentStockPrice(currentStockPrice);
+                Thread.sleep(30000);
+            } catch (InterruptedException e) {
+                // Handle the exception here (e.g., log the exception)
+                e.printStackTrace();
+                // Optionally, you can throw a custom unchecked exception to signal the interruption.
+                // throw new RuntimeException("Thread was interrupted while sleeping.", e);
+            }
+        }
+    }
 }
+
